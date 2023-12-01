f23fecad2015da4ee7160feb5eea22b3673b20a5
Lucas Pereira <lucas_rm35@hotmail.com>
2023-11-30 21:50:04 -0400

    commit
    add comentarios

--- condicionais/refatorandoex03.js
+++ condicionais/refatorandoex03.js
@@ -1,15 +1,16 @@
 let notas = [];
 
-let condicao = ['Reprovado', 'Recuperação', 'Aprovado'];
+let condicao = ['Reprovado', 'Recuperação', 'Aprovado']; //condiçao que o aluno se encontra pela media de notas
 
 
-notas.push(6,8,5);
+notas.push(10,6,10);
 
+// calcula a as notas e gera a media
 let totalGeral = 0;
-for(i = 0; i < notas.length; i++){
+for(i = 0; i < notas.length; i++){ 
     totalGeral += notas[i];
-    media = 10.2//totalGeral / notas.length   
-}
+    media = totalGeral / notas.length   
+};
 
 if(media < 0 || media > 10){
     console.log('[ERRO] O valor não é  valido: Verifica as notas lançadas')
@@ -19,7 +20,7 @@ if(media < 0 || media > 10){
     console.log(`Sua media é de: ${media.toFixed(2)} sua classificação é ${condicao[1]}`);
 }else{
     console.log(`Sua media é de: ${media.toFixed(2)} sua classificação é ${condicao[2]}`);
-}
+};
     
 
 
