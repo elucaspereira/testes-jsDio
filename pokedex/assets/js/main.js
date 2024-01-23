adfe0b791e924a029ad9710c3a41997e2928390b
Lucas Pereira <lucas_rm35@hotmail.com>
2024-01-23 07:07:39 -0400

    commit

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -3,9 +3,40 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <samp class="number">#001</samp>
+            <samp class="name">${pokemon.name}</samp>
+
+            <div class="detail">
+                <ol class="types">
+                    <li class="type">grass</li>
+                    <li class="type">poison</li>
+                </ol>
+
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}">
+            
+            </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))  
-   .catch((error) => console.error(error))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+            
+        }
+    })
+    .catch((error) => console.error(error))
 
 
