6e2c6da04e8bce48d63d4f27812f852808455698
Lucas Pereira <lucas_rm35@hotmail.com>
2024-01-26 21:29:28 -0400

    commit

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,6 +1,4 @@
 
-
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
@@ -24,13 +22,7 @@ function convertPokemonToLi(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = []
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
 
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItems.push(convertPokemonToLi(pokemon))
-    }
-    
-    console.log(listItems);
 })
