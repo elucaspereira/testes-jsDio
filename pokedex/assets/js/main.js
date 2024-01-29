7d9490d50e22d3dafc8be41b92062ad7849ea73e
Lucas Pereira <lucas_rm35@hotmail.com>
2024-01-29 07:35:55 -0400

    pegando o detalhe e img da api

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,17 +1,20 @@
 
+function convertPokemonTypeToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-            <samp class="number">#001</samp>
-            <samp class="name">${pokemon.name}</samp>
+            <span class="number">#${pokemon.order}</span>
+            <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonTypeToLi(pokemon.types).join('')}
                 </ol>
 
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                <img src="${pokemon.sprites.other.dream_world.front_default}"
                     alt="${pokemon.name}">
             
             </div>
@@ -23,6 +26,7 @@ const pokemonList = document.getElementById('pokemonList')
 
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+    const newHtml = pokemons.map(convertPokemonToLi).join('')
+    pokemonList.innerHTML = newHtml
 
 })
