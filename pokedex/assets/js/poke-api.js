ba8c0152c2d665ac56ad54a8c963da7eeb18ebf1
Lucas Pereira <lucas_rm35@hotmail.com>
2024-01-30 07:19:46 -0400

    criando paginação

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -3,7 +3,7 @@ const pokeApi = {};
 
 function convertPokeApiDetailToPokemon(pokeDetail){
     const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.order
+    pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
 
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then((response) => response.json())
         .then(convertPokeApiDetailToPokemon)
 }
-pokeApi.getPokemons = (offset = 0, limit= 10 ) =>{
+pokeApi.getPokemons = (offset = 0, limit= 5 ) =>{
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
